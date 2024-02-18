21d5296460bdac9fde93df439c86361ced0f1d11
Maciek Malik <poczta@maciekmalik.pl>
2024-02-18 06:08:12 +0100

    mocks

--- mock/TokenMockData.js
+++ mock/TokenMockData.js
@@ -2,13 +2,13 @@ const ppraTokens = [
     {
         "id": "1",
         "address": "0x229Fd58132E0be91A2dE414663FcD492baaC8f8a",
-        "isin": "ISIN_1",
+        "isin": "BlockyDevs",
         "series": "SERIES_1"
     },
     {
         "id": "2",
         "address": "0xbB3dbf9AEEb50f7055a95cf1bd71c9DbbeA54F6D",
-        "isin": "ISIN_2",
+        "isin": "DegenHouse",
         "series": "SERIES_2"
     }
 ]
